d848e8c1f9d4191eb97b043b900341ea2d2de22e
Matt Soucy <msoucy@csh.rit.edu>
2018-02-09 17:38:59 -0500

    Small RPS tweaks

--- src/main/java/frc/team166/training/rps/App.java
+++ src/main/java/frc/team166/training/rps/App.java
@@ -3,12 +3,11 @@ package frc.team166.training.rps;
 import frc.team166.training.rps.players.AlwaysPaper;
 import frc.team166.training.rps.players.AlwaysRock;
 import frc.team166.training.rps.players.AlwaysScissors;
-import frc.team166.training.rps.players.Human;
 
 public class App {
     public static void main(String[] args) {
-        Tournament t = new Tournament(5, new AlwaysPaper(), new AlwaysRock(), new AlwaysScissors(), new AlwaysRock(),
-                new AlwaysScissors(), new Human());
-        t.run(true);
+        Tournament t = new Tournament(1001, new AlwaysPaper(), new AlwaysRock(), new AlwaysScissors(), new AlwaysRock(),
+                new AlwaysScissors());
+        t.run();
     }
 }
--- src/main/java/frc/team166/training/rps/Move.java
+++ src/main/java/frc/team166/training/rps/Move.java
@@ -1,5 +1,17 @@
 package frc.team166.training.rps;
 
 public enum Move {
-    Rock, Paper, Scissors
+    Rock, Paper, Scissors;
+
+    public String toEmoji() {
+        switch (this) {
+        case Rock:
+            return "✊";
+        case Paper:
+            return "✋";
+        case Scissors:
+            return "✌";
+        }
+        return null;
+    }
 }
