437fc1657b40c6db896deff8a5574f01486488ab
Matt Soucy <msoucy@csh.rit.edu>
2018-02-11 14:30:02 -0500

    Unifying tournament class hierarchy

---
+++ src/main/java/frc/team166/training/core/TournamentBase.java
@@ -0,0 +1,60 @@
+package frc.team166.training.core;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A tournament takes many players and returns only one
+ * Only the winners survive, and at the end there can be only one
+ */
+public abstract class TournamentBase<Player extends PlayerBase<?, ?>> {
+
+    protected int numMatches;
+    protected Player[] players;
+
+    public TournamentBase(int numMatches, Player... players) {
+        this.numMatches = numMatches;
+        this.players = players;
+        for (Player p : players) {
+            System.out.println(p);
+        }
+    }
+
+    public void run() {
+        run(false);
+    }
+
+    public void run(boolean verbose) {
+        List<Player> roundPlayers = new LinkedList<Player>(Arrays.asList(players));
+        List<Player> nextRoundPlayers = new LinkedList<Player>();
+        int round = 1;
+        while (roundPlayers.size() > 1) {
+            System.out.println("--- ROUND " + round + " ---");
+            while (roundPlayers.size() > 1) {
+                Player p1 = roundPlayers.remove(0);
+                Player p2 = roundPlayers.remove(0);
+                Player winner = runMatchup(p1, p2, verbose);
+                if (winner != null) {
+                    nextRoundPlayers.add(winner);
+                }
+            }
+            if (roundPlayers.size() == 1) {
+                System.out.println("Player " + roundPlayers.get(0) + " gets a bye");
+                nextRoundPlayers.add(roundPlayers.remove(0));
+            }
+            roundPlayers = nextRoundPlayers;
+            Collections.shuffle(roundPlayers);
+            nextRoundPlayers = new LinkedList<Player>();
+            round++;
+        }
+        if (roundPlayers.size() == 0) {
+            System.out.println("There is no winner, blame misbehaving AI");
+        } else {
+            System.out.println("Tournament winner is " + roundPlayers.get(0));
+        }
+    }
+
+    abstract protected Player runMatchup(Player p1, Player p2, boolean verbose);
+}
--- src/main/java/frc/team166/training/rps/Tournament.java
+++ src/main/java/frc/team166/training/rps/Tournament.java
@@ -1,61 +1,22 @@
 package frc.team166.training.rps;
 
 import frc.team166.training.core.MatchStatus;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import frc.team166.training.core.TournamentBase;
 
 /**
  * A tournament takes many players and returns only one
  * Only the winners survive, and at the end there can be only one
  */
-public class Tournament {
-
-    int numMatches;
-    Player[] players;
+public class Tournament extends TournamentBase<Player> {
 
     public Tournament(int numMatches, Player... players) {
-        this.numMatches = numMatches;
-        this.players = players;
+        super(numMatches, players);
     }
 
-    public void run() {
-        run(false);
-    }
-
-    public void run(boolean verbose) {
-        List<Player> roundPlayers = new LinkedList<Player>(Arrays.asList(players));
-        List<Player> nextRoundPlayers = new LinkedList<Player>();
-        int round = 1;
-        while (roundPlayers.size() > 1) {
-            System.out.println("--- ROUND " + round + " ---");
-            while (roundPlayers.size() > 1) {
-                Player p1 = roundPlayers.remove(0);
-                Player p2 = roundPlayers.remove(0);
-                Player winner = runMatchup(p1, p2, verbose);
-                if (winner != null) {
-                    nextRoundPlayers.add(winner);
-                }
-            }
-            if (roundPlayers.size() == 1) {
-                System.out.println("Player " + roundPlayers.get(0) + " gets a bye");
-                nextRoundPlayers.add(roundPlayers.remove(0));
-            }
-            roundPlayers = nextRoundPlayers;
-            Collections.shuffle(roundPlayers);
-            nextRoundPlayers = new LinkedList<Player>();
-            round++;
-        }
-        if (roundPlayers.size() == 0) {
-            System.out.println("There is no winner, blame misbehaving AI");
-        } else {
-            System.out.println("Tournament winner is " + roundPlayers.get(0));
-        }
-    }
-
-    Player runMatchup(Player p1, Player p2, boolean verbose) {
-        Match match = new Match(p1, p2);
+    @Override
+    protected Player runMatchup(Player p1, Player p2, boolean verbose) {
+        Match match = new Match();
+        match.setPlayers(p1, p2);
         int wins = 0;
         for (int i = 0; (i < numMatches || wins == 0) && (i < numMatches * 2); i++) {
             MatchStatus st = match.run(verbose);
